5f8f4368c082c3df10ffe1b3172edd0463e51cce
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-03-15 18:11:26 +0100

    Changes

--- models.py
+++ models.py
@@ -14,13 +14,13 @@ class Author(Document):
 
 class Quote(Document):
     author = ReferenceField(Author, reverse_delete_rule=CASCADE)
-    tags = ListField(StringField(max_length=15))
+    tags = ListField(StringField(max_length=1000))
     quote = StringField()
     meta = {
         "collection": "quotes"
     }
 
-    # def to_json(self, *args, **kwargs):
-    #     data = self.to_mongo(*args, **kwargs)
-    #     data["author"] = self.author.fullname
-    #     return json_util.dumps(data, ensure_ascii=True)
+    def to_json(self, *args, **kwargs):
+        data = self.to_mongo(*args, **kwargs)
+        data["author"] = self.author.fullname
+        return json_util.dumps(data, ensure_ascii=True)
--- seed.py
+++ seed.py
@@ -1,22 +1,64 @@
 import json
 from mongoengine.errors import (NotUniqueError)
 from models import Author, Quote
-
+from mongoengine import connect
 
 if __name__ == '__main__':
-    with open('authors.json', encoding='utf-8') as fd:
-        data = json.load(fd)
-        for el in data:
-            try:
-                author = Author(fullname=el.get('fullname'), born_date=el.get('born_date'), born_location=el.get('born_location'), description=el.get('description'))
-                author.save()
-            except NotUniqueError:
-                print(f"Автор вже існує {el.get('fullname')}")
+    with connect(db='hm', host='mongodb+srv://PythonDB:cud4BUXMwUmTI9A9@cluster0.zv0mgxq.mongodb.net/'):
+        with open('authors.json', encoding='utf-8') as fd:
+            data = json.load(fd)
+            for el in data:
+                try:
+                    author = Author(fullname=el.get('fullname'), born_date=el.get('born_date'), born_location=el.get('born_location'), description=el.get('description'))
+                    author.save()
+                except NotUniqueError:
+                    print(f"Автор вже існує {el.get('fullname')}")
 
+        with open('quotes.json', encoding='utf-8') as fd:
+            data = json.load(fd)
+            for el in data:
+                author = Author.objects(fullname=el.get('author')).first()
+                if author:
+                    quote = Quote(quote=el.get('quote'), tags=el.get('tags'), author=author)
+                    quote.save()
+                else:
+                    print(f"Автор з іменем {el.get('author')} не знайдений")
 
-    with open('quotes.json', encoding='utf-8') as fd:
-        data = json.load(fd)
-        for el in data:
-            author, *_ = Author.objects(fullname=el.get('author'))
-            quote = Quote(quote=el.get('quote'), tags=el.get('tags'), author=author)
-            quote.save()
+# if __name__ == '__main__':
+#     with connect(db='hm', host='mongodb+srv://PythonDB:cud4BUXMwUmTI9A9@cluster0.zv0mgxq.mongodb.net/'):
+#         with open('authors.json', encoding='utf-8') as fd:
+#             data = json.load(fd)
+#             for el in data:
+#                 try:
+#                     author = Author(fullname=el.get('fullname'), born_date=el.get('born_date'), born_location=el.get('born_location'), description=el.get('description'))
+#                     author.save()
+#                 except NotUniqueError:
+#                     print(f"Автор вже існує {el.get('fullname')}")
+#
+#         with open('quotes.json', encoding='utf-8') as fd:
+#             data = json.load(fd)
+#             for el in data:
+#                 author = Author.objects(fullname=el.get('author'))
+#                 if author:
+#                     quote = Quote(quote=el.get('quote'), tags=el.get('tags'), author=author)
+#                     quote.save()
+#                 else:
+#                     print(f"Автор з іменем {el.get('author')} не знайдений")
+
+# if __name__ == '__main__':
+#     with open('authors.json', encoding='utf-8') as fd:
+#         data = json.load(fd)
+#         for el in data:
+#             try:
+#                 author = Author(fullname=el.get('fullname'), born_date=el.get('born_date'), born_location=el.get('born_location'), description=el.get('description'))
+#                 author.save()
+#             except NotUniqueError:
+#                 print(f"Автор вже існує {el.get('fullname')}")
+#
+#
+#     with open('quotes.json', encoding='utf-8') as fd:
+#         data = json.load(fd)
+#         for el in data:
+#             author, *_ = Author.objects(fullname=el.get('author'))
+#             quote = Quote(quote=el.get('quote'), tags=el.get('tags'), author=author)
+#             quote.save()
